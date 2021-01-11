93e76af5123fafc5093c775ef5da16be60cf47a6
AHaliq <liqo12@hotmail.com>
2021-01-11 21:22:21 +0800

    make dnf y to all

--- data/init.sh
+++ data/init.sh
@@ -1,4 +1,4 @@
-dnf update
-dnf install cmake make gcc-c++ wget unzip
+dnf -y update
+dnf -y install cmake make gcc-c++ wget unzip
 wget https://github.com/nus-cs3203/project-wiki/raw/main/CP_StartupSPASolution/CP_StartupSPASolution.zip
 unzip CP_StartupSPASolution.zip
