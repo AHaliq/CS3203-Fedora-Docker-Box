f734c5aaa6226f06027a0468792ecfca34c79a2e
AHaliq <liqo12@hotmail.com>
2021-01-12 11:07:44 +0800

    separate download startup zip

--- data/init.sh
+++ data/init.sh
@@ -1,4 +1,2 @@
 dnf -y update
-dnf -y install cmake make gcc-c++ wget unzip
-wget https://github.com/nus-cs3203/project-wiki/raw/main/CP_StartupSPASolution/CP_StartupSPASolution.zip
-unzip CP_StartupSPASolution.zip
+dnf -y install cmake make gcc-c++
