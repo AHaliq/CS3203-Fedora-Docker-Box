fe7c47f096e8ad04f103195c65c6cc2d591fd058
AHaliq <liqo12@hotmail.com>
2021-01-14 04:39:40 +0800

    undo antlr

--- CS3203/init.sh
+++ CS3203/init.sh
@@ -1,11 +1,5 @@
 dnf -y update
-dnf -y install cmake make gcc-c++ wget unzip java
-
-cd /usr/local/lib
-sudo wget https://www.antlr.org/download/antlr-4.9.1-complete.jar
-export CLASSPATH=".:/usr/local/lib/antlr-4.9.1-complete.jar:$CLASSPATH"
-alias antlr4='java -jar /usr/local/lib/antlr-4.9.1-complete.jar'
-alias grun='java org.antlr.v4.gui.TestRig'
+dnf -y install cmake make gcc-c++ wget unzip
 
 cd /CS3203/data
 wget https://github.com/nus-cs3203/project-wiki/raw/main/CP_StartupSPASolution/CP_StartupSPASolution.zip
