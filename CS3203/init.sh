5e65352ec5ec0830b08129aebbf3f28f2984b1b3
AHaliq <liqo12@hotmail.com>
2021-01-19 14:12:56 +0800

    clone repo

--- CS3203/init.sh
+++ CS3203/init.sh
@@ -1,9 +1,11 @@
 dnf -y update
-dnf -y install cmake make gcc-c++ wget unzip
+dnf -y install cmake make gcc-c++ wget unzip git
 
 cd /CS3203/data
 wget https://github.com/nus-cs3203/project-wiki/raw/main/CP_StartupSPASolution/CP_StartupSPASolution.zip
 unzip -o CP_StartupSPASolution.zip
 rm -rf CP_StartupSPASolution.zip
+
+git clone https://github.com/AHaliq/T07_CS3203
 
 cd /CS3203
