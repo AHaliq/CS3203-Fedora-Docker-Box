6e8259d7f5c5052ad7a27ef47e80d244e1004c87
AHaliq <liqo12@hotmail.com>
2021-01-19 14:39:42 +0800

    create dir if dont exist

--- CS3203/init.sh
+++ CS3203/init.sh
@@ -1,6 +1,8 @@
 dnf -y update
 dnf -y install cmake make gcc-c++ wget unzip
 
+mkdir -p /CS3203/data
+
 cd /CS3203/data
 wget https://github.com/nus-cs3203/project-wiki/raw/main/CP_StartupSPASolution/CP_StartupSPASolution.zip
 unzip -o CP_StartupSPASolution.zip
